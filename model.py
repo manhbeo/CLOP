71ad0823992ba53491241d95effc0f3a824dd519
manhbeo <kozmo.burnoid@gmail.com>
2024-08-19 16:08:28 +0700

    x

--- model.py
+++ model.py
@@ -160,7 +160,7 @@ class CLOA(pl.LightningModule):
             optimizer = LARS(self.parameters(), lr=self.learning_rate, weight_decay=1e-6)
 
         if self.lr_schedule == "cosine":    
-            self.scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=1000)
+            self.scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=1000) #TODO: get some warm up
         elif self.lr_schedule == "linear":
             self.scheduler = torch.optim.lr_scheduler.LinearLR(optimizer, total_iters=500)
         elif self.lr_schedule == "exp":
