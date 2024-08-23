f0af3d807544517305da99233d095be25dffe51a
manhbeo <kozmo.burnoid@gmail.com>
2024-08-23 15:12:23 +0700

    x

--- model.py
+++ model.py
@@ -76,7 +76,7 @@ class CLOA(pl.LightningModule):
 
         self.projection_head = SimCLRProjectionHead(output_dim=self.output_dim)
 
-        self.learning_rate = 0.075 * math.sqrt(batch_size) if dataset.startswith("cifar") else 0.3 * batch_size/256
+        self.learning_rate = 0.075 * math.sqrt(batch_size)
         self.feature_bank_size = 1024
         self._init_feature_bank(self.feature_bank_size)
     
