c28aa218294c078d7ade34bfc9f6ed1ce47618fa
manhbeo <kozmo.burnoid@gmail.com>
2024-08-27 00:31:14 +0700

    x

--- data_module.py
+++ data_module.py
@@ -104,7 +104,6 @@ class CustomDataModule(pl.LightningDataModule):
         self.data_dir = data_dir + "_" + dataset
         self.batch_size = batch_size
         self.dataset = dataset
-        normalize = None
 
         if self.dataset.startswith("cifar"):
             if self.dataset == "cifar10":
@@ -167,7 +166,6 @@ class CustomEvaluationDataModule(pl.LightningDataModule):
         self.data_dir = data_dir + "_" + dataset
         self.batch_size = batch_size
         self.dataset = dataset
-        normalize = None
 
         if self.dataset.startswith("cifar"):
             if self.dataset == "cifar10":
