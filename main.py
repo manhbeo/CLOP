005facc06a143f3f7a1b8434b08231620541edbd
manhbeo <kozmo.burnoid@gmail.com>
2024-08-27 21:04:05 +0700

    x

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def train(epochs, batch_size, dataset, pretrain_dir = None, OAR=True, OAR_only=F
         model = CLOA(batch_size, dataset, OAR, OAR_only, supervised, devices)
     
     data_module = CustomDataModule(batch_size=batch_size, dataset=dataset)
-    wandb_logger = pl.loggers.WandbLogger(project="CLOA_Train", name=f'{dataset}-{batch_size*devices}-oar:{OAR}-only:{OAR_only}')
+    wandb_logger = pl.loggers.WandbLogger(project="CLOA_Train", name=f'{dataset}-{batch_size*devices}-oar:{OAR}')
 
     #next use iNaturalist
     checkpoint_callback = ModelCheckpoint(
@@ -44,7 +44,7 @@ def train(epochs, batch_size, dataset, pretrain_dir = None, OAR=True, OAR_only=F
 def eval(pretrain_dir, batch_size, epochs, dataset, OAR, OAR_only):
     model = CLOA.load_from_checkpoint(pretrain_dir)
     data_module = CustomEvaluationDataModule(batch_size=batch_size, dataset=dataset)
-    wandb_logger = pl.loggers.WandbLogger(project="CLOA_Eval", name=f'{dataset}-oar:{OAR}-only:{OAR_only}')
+    wandb_logger = pl.loggers.WandbLogger(project="CLOA_Eval", name=f'{dataset}-oar:{OAR}')
     if dataset == "cifar10": 
         num_classes = 10
         feature_dim = 128
