c68663bb44ca84f8c22ee2e4b065cb8a8f196cae
manhbeo <kozmo.burnoid@gmail.com>
2024-08-28 14:03:07 +0700

    x

--- data_module.py
+++ data_module.py
@@ -106,7 +106,7 @@ class CustomDataModule(pl.LightningDataModule):
             elif self.dataset == "cifar100":
                 normalize = transforms.Normalize(mean=[0.5071, 0.4867, 0.4408], std=[0.2675, 0.2565, 0.2761])
             self.train_transform = transforms.Compose([
-                transforms.RandomResizedCrop(32, scale=(0.08, 1.0), ratio=(3/4, 4/3)),
+                transforms.RandomResizedCrop(32),
                 transforms.RandomHorizontalFlip(),
                 # transforms.AutoAugment(policy=transforms.AutoAugmentPolicy.CIFAR10),
                 transforms.RandomApply([
@@ -119,7 +119,7 @@ class CustomDataModule(pl.LightningDataModule):
         elif self.dataset == "imagenet":
             normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
             self.train_transform = transforms.Compose([
-                transforms.RandomResizedCrop(224, scale=(0.08, 1.0), ratio=(3/4, 4/3)),
+                transforms.RandomResizedCrop(224),
                 # transforms.Resize(256),  # Resize the shorter side to 256
                 # transforms.CenterCrop(224),
                 transforms.RandomHorizontalFlip(),
@@ -175,7 +175,7 @@ class CustomEvaluationDataModule(pl.LightningDataModule):
             elif self.dataset == "cifar100":
                 normalize = transforms.Normalize(mean=[0.5071, 0.4867, 0.4408], std=[0.2675, 0.2565, 0.2761])
             self.train_transform = transforms.Compose([
-                transforms.RandomResizedCrop(32, scale=(0.08, 1.0), ratio=(3/4, 4/3)),
+                transforms.RandomResizedCrop(32),
                 transforms.RandomHorizontalFlip(),
                 # transforms.AutoAugment(policy=transforms.AutoAugmentPolicy.CIFAR10),
                 transforms.RandomApply([
@@ -188,7 +188,7 @@ class CustomEvaluationDataModule(pl.LightningDataModule):
         elif self.dataset == "imagenet":
             normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
             self.train_transform = transforms.Compose([
-                transforms.RandomResizedCrop(224, scale=(0.08, 1.0), ratio=(3/4, 4/3)),
+                transforms.RandomResizedCrop(224),
                 # transforms.Resize(256),
                 # transforms.CenterCrop(224),
                 transforms.RandomHorizontalFlip(),
