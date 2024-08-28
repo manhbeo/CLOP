fa26dc975a7ffd7d7a874bf06961b3b347e09bb8
manhbeo <kozmo.burnoid@gmail.com>
2024-08-28 20:55:57 +0700

    x

--- data_module.py
+++ data_module.py
@@ -106,7 +106,7 @@ class CustomDataModule(pl.LightningDataModule):
             elif self.dataset == "cifar100":
                 normalize = transforms.Normalize(mean=[0.5071, 0.4867, 0.4408], std=[0.2675, 0.2565, 0.2761])
             self.train_transform = transforms.Compose([
-                transforms.RandomResizedCrop(32),
+                transforms.RandomResizedCrop(32, scale=(0.2, 1.0)),
                 transforms.RandomHorizontalFlip(),
                 # transforms.AutoAugment(policy=transforms.AutoAugmentPolicy.CIFAR10),
                 transforms.RandomApply([
@@ -175,7 +175,7 @@ class CustomEvaluationDataModule(pl.LightningDataModule):
             elif self.dataset == "cifar100":
                 normalize = transforms.Normalize(mean=[0.5071, 0.4867, 0.4408], std=[0.2675, 0.2565, 0.2761])
             self.train_transform = transforms.Compose([
-                transforms.RandomResizedCrop(32),
+                transforms.RandomResizedCrop(32, scale=(0.2, 1.0)),
                 transforms.RandomHorizontalFlip(),
                 # transforms.AutoAugment(policy=transforms.AutoAugmentPolicy.CIFAR10),
                 transforms.RandomApply([
