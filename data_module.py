4d84fe7a721c6e5cefa60f439447c13e1349945d
manhbeo <kozmo.burnoid@gmail.com>
2024-08-30 01:20:17 +0700

    x

--- data_module.py
+++ data_module.py
@@ -109,7 +109,7 @@ class CustomDataModule(pl.LightningDataModule):
                 transforms.RandomHorizontalFlip(),
                 # transforms.AutoAugment(policy=transforms.AutoAugmentPolicy.CIFAR10),
                 transforms.RandomApply([
-                    transforms.ColorJitter(0.5, 0.5, 0.5, 0.1)
+                    transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)
                 ], p=0.8),
                 transforms.RandomGrayscale(p=0.2),
                 # transforms.RandomApply([transforms.GaussianBlur(kernel_size=int(32 * 0.1), sigma=(0.1, 2.0))], p=0.5),
@@ -165,56 +165,15 @@ class CustomEvaluationDataModule(pl.LightningDataModule):
         self.batch_size = batch_size
         self.dataset = dataset
 
-        # if self.dataset.startswith("cifar"):
-        #     if self.dataset == "cifar10":
-        #         normalize = transforms.Normalize(mean=[0.5071, 0.4865, 0.4409], std=[0.2673, 0.2564, 0.2762])
-        #     elif self.dataset == "cifar100":
-        #         normalize = transforms.Normalize(mean=[0.5071, 0.4867, 0.4408], std=[0.2675, 0.2565, 0.2761])
-        # elif self.dataset == "imagenet":
-        #     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-
-        # self.transform = transforms.Compose([
-        #     transforms.Resize(32 if self.dataset.startswith("cifar") else 256),
-        #     transforms.CenterCrop(32 if self.dataset.startswith("cifar") else 224),
-        #     transforms.ToTensor(),
-        #     normalize
-        # ])
-
         if self.dataset.startswith("cifar"):
             if self.dataset == "cifar10":
                 normalize = transforms.Normalize(mean=[0.5071, 0.4865, 0.4409], std=[0.2673, 0.2564, 0.2762])
             elif self.dataset == "cifar100":
                 normalize = transforms.Normalize(mean=[0.5071, 0.4867, 0.4408], std=[0.2675, 0.2565, 0.2761])
-            self.train_transform = transforms.Compose([
-                transforms.RandomResizedCrop(32),
-                transforms.RandomHorizontalFlip(),
-                # transforms.AutoAugment(policy=transforms.AutoAugmentPolicy.CIFAR10),
-                transforms.RandomApply([
-                    transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)
-                ], p=0.8),
-                transforms.RandomGrayscale(p=0.2),
-                # transforms.RandomApply([transforms.GaussianBlur(kernel_size=int(32 * 0.1), sigma=(0.1, 2.0))], p=0.5),
-                transforms.ToTensor(),
-                normalize,
-            ])
         elif self.dataset == "imagenet":
             normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-            self.train_transform = transforms.Compose([
-                transforms.RandomResizedCrop(224),
-                # transforms.Resize(256),
-                # transforms.CenterCrop(224),
-                transforms.RandomHorizontalFlip(),
-                # transforms.AutoAugment(policy=transforms.AutoAugmentPolicy.IMAGENET),
-                transforms.RandomApply([
-                    transforms.ColorJitter(0.8, 0.8, 0.8, 0.2)
-                ], p=0.8),
-                transforms.RandomGrayscale(p=0.2),
-                transforms.RandomApply([transforms.GaussianBlur(kernel_size=int(224 * 0.1)+1, sigma=(0.1, 2.0))], p=0.5),
-                transforms.ToTensor(),
-                normalize
-            ])
 
-        self.val_transform = transforms.Compose([
+        self.transform = transforms.Compose([
             transforms.Resize(32 if self.dataset.startswith("cifar") else 256),
             transforms.CenterCrop(32 if self.dataset.startswith("cifar") else 224),
             transforms.ToTensor(),
@@ -226,8 +185,8 @@ class CustomEvaluationDataModule(pl.LightningDataModule):
             self.train_dataset = datasets.CIFAR10(self.data_dir, train=True, transform=self.transform, download=True)
             self.val_dataset = datasets.CIFAR10(self.data_dir, train=False, transform=self.transform, download=True)
         elif self.dataset == "cifar100":
-            self.train_dataset = datasets.CIFAR100(self.data_dir, train=True, transform=self.val_transform, download=True)
-            self.val_dataset = datasets.CIFAR100(self.data_dir, train=False, transform=self.val_transform, download=True)
+            self.train_dataset = datasets.CIFAR100(self.data_dir, train=True, transform=self.transform, download=True)
+            self.val_dataset = datasets.CIFAR100(self.data_dir, train=False, transform=self.transform, download=True)
         elif self.dataset == "imagenet":
             self.train_dataset = datasets.ImageNet(self.data_dir, split='train', transform=self.transform)
             self.val_dataset =  datasets.ImageNet(self.data_dir, split='val', transform=self.transform)
