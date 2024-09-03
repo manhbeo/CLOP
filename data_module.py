c7090d6a2fd856133eb81606e25f332bdfd6aec3
manhbeo <kozmo.burnoid@gmail.com>
2024-09-03 13:10:46 -0500

    x

--- data_module.py
+++ data_module.py
@@ -117,15 +117,15 @@ class CustomDataModule(pl.LightningDataModule):
         elif self.dataset == "imagenet":
             normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
             self.train_transform = transforms.Compose([
-                # transforms.RandomResizedCrop(224),
-                transforms.Resize(224),
-                # transforms.RandomHorizontalFlip(),
-                transforms.AutoAugment(policy=transforms.AutoAugmentPolicy.IMAGENET),
-                # transforms.RandomApply([
-                #     transforms.ColorJitter(0.8, 0.8, 0.8, 0.2)
-                # ], p=0.8),
-                # transforms.RandomGrayscale(p=0.2),
-                # transforms.RandomApply([transforms.GaussianBlur(kernel_size=int(224 * 0.1)+1, sigma=(0.1, 2.0))], p=0.5),
+                transforms.RandomResizedCrop(224),
+                # transforms.Resize(224),
+                transforms.RandomHorizontalFlip(),
+                # transforms.AutoAugment(policy=transforms.AutoAugmentPolicy.IMAGENET),
+                transforms.RandomApply([
+                    transforms.ColorJitter(0.8, 0.8, 0.8, 0.2)
+                ], p=0.8),
+                transforms.RandomGrayscale(p=0.2),
+                transforms.RandomApply([transforms.GaussianBlur(kernel_size=int(224 * 0.1)+1, sigma=(0.1, 2.0))], p=0.5),
                 transforms.ToTensor(),
                 normalize
             ])
@@ -182,15 +182,15 @@ class CustomEvaluationDataModule(pl.LightningDataModule):
         elif self.dataset == "imagenet":
             normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
             self.train_transform = transforms.Compose([
-                # transforms.RandomResizedCrop(224),
-                transforms.Resize(224),
-                # transforms.RandomHorizontalFlip(),
-                transforms.AutoAugment(policy=transforms.AutoAugmentPolicy.IMAGENET),
-                # transforms.RandomApply([
-                #     transforms.ColorJitter(0.8, 0.8, 0.8, 0.2)
-                # ], p=0.8),
-                # transforms.RandomGrayscale(p=0.2),
-                # transforms.RandomApply([transforms.GaussianBlur(kernel_size=int(224 * 0.1)+1, sigma=(0.1, 2.0))], p=0.5),
+                transforms.RandomResizedCrop(224),
+                # transforms.Resize(224),
+                transforms.RandomHorizontalFlip(),
+                # transforms.AutoAugment(policy=transforms.AutoAugmentPolicy.IMAGENET),
+                transforms.RandomApply([
+                    transforms.ColorJitter(0.8, 0.8, 0.8, 0.2)
+                ], p=0.8),
+                transforms.RandomGrayscale(p=0.2),
+                transforms.RandomApply([transforms.GaussianBlur(kernel_size=int(224 * 0.1)+1, sigma=(0.1, 2.0))], p=0.5),
                 transforms.ToTensor(),
                 normalize
             ])
