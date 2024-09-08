eeb0fff8b871a8127a4ce675666e59b3f28c375a
manhbeo <kozmo.burnoid@gmail.com>
2024-09-08 06:58:59 -0500

    x

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ from pytorch_lightning import seed_everything
 from linear_classifier import LinearClassifier
 import torch.nn as nn
 
-def train(epochs, batch_size, dataset, pretrain_dir = None, OAR=True, loss="nxt_ent", devices=1, k=100, num_workers=9, distance="cosine", augment="auto_imgnet"):
+def train(epochs, batch_size, dataset, pretrain_dir = None, OAR=True, loss="nxt_ent", devices=1, k=100, num_workers=9, distance="cosine", augment="auto_imgnet", lr=None):
     if pretrain_dir != None:
         model = CLOA.load_from_checkpoint(pretrain_dir)
     else: 
-        model = CLOA(batch_size, dataset, OAR, loss, devices, k)
+        model = CLOA(batch_size, dataset, OAR, loss, devices, k, distance, lr) #train from scratch, since this something we don't want
     
     data_module = CustomDataModule(batch_size=batch_size, dataset=dataset, num_workers=num_workers, augment=augment)
     wandb_logger = pl.loggers.WandbLogger(project="CLOA_Train", name=f'{dataset}-{batch_size*devices}-oar={OAR}-dis={distance}')
@@ -112,6 +112,7 @@ if __name__ == '__main__':
     parser.add_argument("--epochs", type=int)
     parser.add_argument("--pretrain_dir", type=str)
     parser.add_argument("--batch_size", type=int)
+    parser.add_argument("--lr", type=float)
     parser.add_argument("--devices", type=int, default=1)
     parser.add_argument("-k", type=int, default=100)
     parser.add_argument("--num_workers", type=int, default=9)
@@ -130,4 +131,4 @@ if __name__ == '__main__':
     elif args.test:
         test(args.pretrain_dir, args.batch_size, args.dataset, args.num_workers)
     else:
-        train(args.epochs, args.batch_size, args.dataset, args.pretrain_dir, args.OAR, args.loss, args.devices, args.k, args.num_workers, args.distance, args.augment)
+        train(args.epochs, args.batch_size, args.dataset, args.pretrain_dir, args.OAR, args.loss, args.devices, args.k, args.num_workers, args.distance, args.augment, args.lr)
