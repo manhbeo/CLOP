2b51662c3898c3ee64ae496bde825efd63a2774f
manhbeo <kozmo.burnoid@gmail.com>
2024-09-10 11:15:36 -0500

    x

--- main.py
+++ main.py
@@ -122,7 +122,7 @@ if __name__ == '__main__':
     parser.add_argument("-k", type=int, default=100)
     parser.add_argument("--num_workers", type=int, default=9)
     parser.add_argument("--dataset", type=str)
-    parser.add_argument("--distance", type=str)
+    parser.add_argument("--distance", type=str, default="cosine")
     parser.add_argument("--augment", type=str)
     parser.add_argument("--loss", type=str)
     parser.add_argument("--OAR", action='store_true')
