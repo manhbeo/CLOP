1054a6d268779701e3ba647a67519cf5a27f2240
manhbeo <kozmo.burnoid@gmail.com>
2024-09-13 14:44:13 -0500

    temp=0.2

--- model.py
+++ model.py
@@ -68,7 +68,8 @@ class CLOA(pl.LightningModule):
             elif dataset == "cifar100": 
                 temperature = 0.2
             elif dataset == "imagenet" or dataset == "tiny_imagenet":
-                temperature = 0.1
+                #TODO: change this
+                temperature = 0.2
             if self.loss == "ntx_ent":
                 self.criterion = NTXentLoss(temperature=temperature, gather_distributed=True)
             elif self.loss == "supcon":
