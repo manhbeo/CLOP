1654f4f205211c7ad4b42497d044aabfe9d135e0
manhbeo <kozmo.burnoid@gmail.com>
2024-09-13 23:24:37 -0500

    x

--- model.py
+++ model.py
@@ -166,7 +166,7 @@ class CLOA(pl.LightningModule):
         optimizer = LARS(self.parameters(), lr=self.learning_rate, weight_decay=1e-6)
         self.scheduler = CosineWarmupScheduler(
                 optimizer=optimizer,
-                warmup_epochs=0 if self.dataset.startswith("cifar") else 0,
+                warmup_epochs=0 if self.dataset.startswith("cifar") else 10,
                 max_epochs=int(self.trainer.estimated_stepping_batches),
             )
 
