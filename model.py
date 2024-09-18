e93525f471d8824b9b2c140338bfeb780fbeccd3
manhbeo <kozmo.burnoid@gmail.com>
2024-09-18 09:55:28 -0500

    x

--- model.py
+++ model.py
@@ -90,7 +90,10 @@ class CLOA(pl.LightningModule):
             if dataset.startswith("cifar"):
                 self.learning_rate = 0.075 * math.sqrt(batch_size*devices)
             elif dataset == "tiny_imagenet":
-                self.learning_rate = 0.7 * (batch_size*devices) / 256
+                if self.loss == "supcon":
+                    self.learning_rate = 0.7 * (batch_size*devices) / 256
+                elif self.loss == "ntx_ent":
+                    self.learning_rate = 0.3 * (batch_size*devices) / 256
         else:
             self.learning_rate = learning_rate
         self.feature_bank_size = 2048
